c107a78666bd0eb6b506b3b664dd4a8860f88869
alexlbt <lambertalexis2@gmail.com>
2015-03-18 12:49:20 +0100

    BORDEL DE BEERS BORDEL C'EST BON

--- js/bundle.js
+++ js/bundle.js
@@ -699,11 +699,17 @@ module.exports=function($scope,config,$location){
 };
 },{}],17:[function(require,module,exports){
 module.exports=function() {
-	var factory={breweries:{},server:{}};
+	var factory={breweries:{}, beers:{},server:{}};
 	factory.activeBrewery=undefined;
 	factory.breweries.loaded=false;
 	factory.breweries.refresh="all";//all|ask
 	factory.breweries.update="immediate";//deffered|immediate
+	
+	factory.beers.loaded=false;
+	factory.beers.refresh="all";//all|ask
+	factory.beers.update="immediate";//deffered|immediate
+	
+	
 	factory.server.privateToken="";
 	factory.server.restServerUrl="http://127.0.0.1/rest-open-beer/";
 	factory.server.force=false;
