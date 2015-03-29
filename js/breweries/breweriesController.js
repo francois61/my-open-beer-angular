d7929c1e9d82191dbe3943642478d3a5fb9506ad
Lambert <lambertalexis2@gmail.com>
2015-03-29 18:38:40 +0200

    brasseries

--- js/breweries/breweriesController.js
+++ js/breweries/breweriesController.js
@@ -27,6 +27,18 @@ module.exports=function($scope,rest,$timeout,$location,config,$route,save) {
 	$scope.showUpdate=function(){
 		return angular.isDefined($scope.activeBrewery);
 	};
+
+    $scope.showDetails=function(){
+        return angular.isDefined($scope.activeBrewery);
+    };
+    
+    $scope.details=function(brewery){
+        if(angular.isDefined(brewery))
+            $scope.activeBrewery=brewery;
+        config.activeBrewery=angular.copy($scope.activeBrewery);
+        config.activeBrewery.reference=$scope.activeBrewery;
+        $location.path("breweries/details");
+    };
 	
 	$scope.refreshOnAsk=function(){
 		return config.breweries.refresh == 'ask';
@@ -80,14 +92,6 @@ module.exports=function($scope,rest,$timeout,$location,config,$route,save) {
 	};
 	
 	$scope.edit=function(brewery){
-		if(angular.isDefined(brewery))
-			$scope.activeBrewery=brewery;
-		config.activeBrewery=angular.copy($scope.activeBrewery);
-		config.activeBrewery.reference=$scope.activeBrewery;
-		$location.path("breweries/update");
-	}
-
-	$scope.see=function(brewery){
 		if(angular.isDefined(brewery))
 			$scope.activeBrewery=brewery;
 		config.activeBrewery=angular.copy($scope.activeBrewery);
--- js/breweries/breweryDetailController.js
+++ js/breweries/breweryDetailController.js
@@ -1,5 +1,11 @@
 module.exports=function($scope,config,$location,rest,save,$document,modalService, $controller){
-    
+
+    if (config.activeBrewery==null || config.activeBrewery =="")
+        $scope.brewery = $routeParams
+    else
+        $scope.brewery = config.activeBrewery;
+
+   
 
 
 };
